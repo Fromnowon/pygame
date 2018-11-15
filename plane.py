018d56d91267ad75bf99138b277f6f7a413dcc72
Fromnowon <zihangzhao@gmail.com>
2018-11-15 17:04:53 +0800

    update

--- plane.py
+++ plane.py
@@ -1,1 +1,11 @@
 import pygame
+import os
+
+pygame.init()
+screen = pygame.display.set_mode((480, 852))
+bg = pygame.image.load('./Assert/images/background.png')
+screen.blit(bg, (0, 0))
+pygame.display.update()
+os.system("pause")
+# if input():
+#     pygame.quit()
